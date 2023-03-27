68fc0ac2e214860a7aa8a4e9c560a816400f5549
raneemalhabib <raneemalh111@gmail.com>
2023-03-27 13:15:09 +0300

    After applying second page

--- app/src/main/java/com/example/lab2/MainActivity.java
+++ app/src/main/java/com/example/lab2/MainActivity.java
@@ -2,12 +2,14 @@ package com.example.lab2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-button b;
+Button b;
 EditText e ;
 
 
@@ -19,19 +21,13 @@ EditText e ;
         e = findViewById(R.id.personname);
 
 
-        b.setOnClickListener(v -> {
+         b.setOnClickListener(v -> {
 
 
+            String str = e.getText().toString();
+            Intent intent = new Intent(getApplicationContext(), Secondactivity.class);
+            intent.putExtra("personname", str);
+            startActivity(intent);
 
 
-        }
-
-
-
-
-
-
-
-
-    }
-}
+        } ); }}
--- app/src/main/java/com/example/lab2/Secondactivity.java
+++ app/src/main/java/com/example/lab2/Secondactivity.java
@@ -2,13 +2,24 @@ package com.example.lab2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Secondactivity extends AppCompatActivity {
-
+TextView tv ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_secondactivity);
+
+        tv= findViewById(R.id.textView);
+        Intent i = getIntent();
+        String str = i.getStringExtra("personname");
+        tv.setText("Hello, " + str +"!");
+
+
+
+
     }
 }
